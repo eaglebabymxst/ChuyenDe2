2a274b9e8d60406a7f5800b5ea64d1803dc7c683
khanhdeptrai <46476419+eaglebabymxst@users.noreply.github.com>
2022-04-12 13:48:15 +0700

    ganxong

--- js/jsdangnhap.js
+++ js/jsdangnhap.js
@@ -1,3 +1,21 @@
-$(".tieptheo").on('click',function () {
-    console.log($("#ngaysinh").val());
+$(".dangnhap").on('click',function () {
+        $(".login").removeClass("is-hidden");
+})
+$(".btn_close").on('click',function () {
+    $(".login").addClass("is-hidden");
+})
+$(".ttformtt").on('click',function () {
+    $(".formThongTin").addClass("is-hidden");
+    $(".formLyLich").addClass("is-hidden");
+    $(".formAnh").removeClass("is-hidden");
+})
+$(".tvformanh").on('click',function () {
+    $(".formAnh").addClass("is-hidden");
+    $(".formLyLich").addClass("is-hidden");
+    $(".formThongTin").removeClass("is-hidden");
+})
+$(".ttformanh").on('click',function () {
+    $(".formAnh").addClass("is-hidden");
+    $(".formLyLich").removeClass("is-hidden");
+    $(".formThongTin").addClass("is-hidden");
 })
