2503ca98aa44077651e73640a91682ebe7726e76
khanhdeptrai <46476419+eaglebabymxst@users.noreply.github.com>
2022-04-16 00:53:45 +0700

    upload PDF xong

--- js/XuatPDF.js
+++ js/XuatPDF.js
@@ -1,4 +1,4 @@
-import { myFont } from './myFont.js';
+import {myFont} from './myFont.js';
 $(".xacnhan").on('click',function () {
     var doc = new jsPDF();
     var img= new Image()
@@ -9,5 +9,14 @@ $(".xacnhan").on('click',function () {
     doc.text(10,10,"Hồ Sơ Đăng Kí")
     doc.text(10,20,"Huỳnh Duy Khánh")
     doc.addImage(img, 'jpg', 10, 40,30,30);
-    doc.save();
+    var blob = doc.output('blob');
+    Upload(blob) 
 })
+async function Upload(blob) {
+    let formData = new FormData();           
+    formData.append("pdf", blob);
+    await fetch('./php/upload.php', {
+        method: "POST", 
+        body: formData,
+    }).then(response => response.text());
+}
